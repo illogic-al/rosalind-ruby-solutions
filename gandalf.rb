bdd33aadb77ccb612864ca01de82bb00269e8222
Orville Bennett <illogical1@gmail.com>
2013-01-18 13:16:29 -0500

    Make it clear exactly who's speaking here

--- gandalf.rb
+++ gandalf.rb
@@ -1,17 +1,17 @@
 module Gandalf
   def self.the_validator(input)
     if File.file?(input)
-      puts "You've been sent back until your task is done."
+      puts "Gandalf says:\n\t\"I've been sent back until my task is done.\""
       input = File.read(input)
     elsif input.is_a?(String)
-      puts "The board is set... the pieces are moving."
+      puts "Gandalf says:\n\t\"The board is set... the pieces are moving.\""
       input
     else
-      message = "You are not a String! You are not a File!"
+      message = "\"You are not a String! You are not a File!"
       message += "\n The dark fire will not avail you, unknown entity!"
       message += " Go back to the shadow."
-      message += "\n YOU! SHALL NOT! PASS!"
-      abort(message)
+      message += "\n YOU! SHALL NOT! PASS!\""
+      abort(message) # we should never ever see this bit of ridiculousness
     end
   end
 end
