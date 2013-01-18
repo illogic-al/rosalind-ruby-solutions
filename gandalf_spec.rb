54c407b1795f9c002705a58c16e0017f219223c6
Orville Bennett <illogical1@gmail.com>
2013-01-18 16:00:53 -0500

    Specify

--- gandalf_spec.rb
+++ gandalf_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative 'gandalf'
 
 describe Gandalf do
-  it "takes our input and checks if it is a file"
+  it "takes a string parameter and checks if it is a file"
 
   it "takes our input and checks if it is a string"
 
