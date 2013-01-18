38538874b320f3f9cef6bf36ad17ec29a3decac8
Orville Bennett <illogical1@gmail.com>
2013-01-18 13:38:10 -0500

    Put up skeleton for Gandalf's spec. Need more RSPEC-fu to test for truthiness.

---
+++ gandalf_spec.rb
@@ -0,0 +1,7 @@
+describe Gandalf do
+  it "takes our input and checks if it is a file"
+
+  it "takes our input and checks if it is a string"
+
+  it "dies if we have neither File nor String as input"
+end
