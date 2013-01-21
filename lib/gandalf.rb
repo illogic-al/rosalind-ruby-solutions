dd02e221638ec5df7a00cf66e90dcd54aac65f27
Orville Bennett <illogical1@gmail.com>
2013-01-21 15:10:16 -0500

    Refactor Gandalf with a bit more intelligence. Now strips strings. Also looks for valid nucleotides or common placeholders for them.

--- lib/gandalf.rb
+++ lib/gandalf.rb
@@ -1,17 +1,21 @@
 module Gandalf
   def self.the_validator(input)
+    # We'll eat nummy newlines and other whitespace with 'strip' method
+    # after testing them. Only the virtuous shall pass.
+
     if File.file?(input)
-      puts "Gandalf says:\n\t\"I've been sent back until my task is done.\""
-      input = File.read(input).chomp
-    elsif input.is_a?(String)
-      puts "Gandalf says:\n\t\"The board is set... the pieces are moving.\""
-      input
-    else
-      message = "\"You are not a String! You are not a File!"
-      message += "\n The dark fire will not avail you, unknown entity!"
-      message += " Go back to the shadow."
-      message += "\n YOU! SHALL NOT! PASS!\""
-      abort(message) # we should never ever see this bit of ridiculousness
+      input = File.read(input).strip
+    end
+
+    if input.is_a?(String) # die if we contain anything other than the values below
+      if input.gsub(/[ACGTUNXacgtunx-]/, "").strip.empty?
+        puts "Gandalf says:\n\t\"The board is set... the pieces are moving.\""
+        input.strip
+      else
+        message = "You have characters that aren't valid nucleotides."
+        message << "\nYou! Shall not! PAAAAAAAAAAAAAAAAAAAAAASS!"
+        abort(message) 
+      end
     end
   end
 end
